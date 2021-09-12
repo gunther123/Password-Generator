7198a96e9a50c19c86d07fb3401c0da70d540724
Steven Slocum <steven.t.slocum@gmail.com>
2021-09-11 19:01:45 -0500

    added prompts for upper and lower case characters being allowed in generated password

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,24 +5,55 @@
 var generatePassword = function(){
 
   //Defining all possible characters for password
-  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-  var numerics = "1234567890";
-  var specialChar = "!'`~@#$%^&*()_+-][}{|><?;:=";
+  var passwordChars = {
+    upperCase:"ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    lowerCase:"abcdefghijklmnopqrstuvwxyz",
+    numerics:"1234567890",
+    specialChar:"!'`~@#$%^&*()_+-][}{|><?;:=",
+  }
 
   //Defining Password minimum and maximum password size limit
   var passwordLimit = [8,128];
 
   //Prompt user to choose password length
-  var passwordLength = prompt("Please choose your password length between 8-128 characters.")
+  var passwordLength = prompt("Please choose your password length between 8-128 characters.");
 
+  //Defining container for characters to be used in password
+  var passwordContainer = "";
+ 
   //Turn passwordLength into integer instead of string
   parseInt(passwordLength);
 
-  if (passwordLength < passwordLimit[0] | passwordLimit > passwordLimit[1] ){
+  //Check if user selected an acceptable password length
+  if (passwordLength < passwordLimit[0] | passwordLength > passwordLimit[1] ){
     alert("Please choose a valid length for your password.")
     return generatePassword();
   }
+
+  //Prompt user to choose to use uppercase characters or not
+  var useUpperCase = confirm("Would you like uppercase characters in your password?\
+  \nClick Ok to add uppercase characters to your password.\
+  \nClick Cancel to not use uppercase characters in your password.")
+
+  //Add uppercase characters to list of characters to be used
+  if(useUpperCase === true){
+    passwordContainer = passwordContainer + passwordChars.upperCase;
+    console.log(passwordContainer);
+  }
+  //Prompt user to choose to use lowercase characters or not
+  var useLowerCase = confirm("Would you like lowercase characters in your password?\
+  \nClick Ok to add lowercase characters to your password.\
+  \nClick Cancel to not use lowercase characters in your password.")
+
+  //Add lowercase characters to list of characters to be used
+  if(useLowerCase === true){
+    passwordContainer = passwordContainer + passwordChars.lowerCase;
+    console.log(passwordContainer);
+  }
+
+  
+
+
 
 
 
